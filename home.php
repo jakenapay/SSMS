f98535c3d9a5714580ba1178d4edb5779a3086b8
Jake Napay <jakemantesnapay@gmail.com>
2023-05-17 22:24:15 +0800

    Icon in home tab added

--- home.php
+++ home.php
@@ -30,7 +30,8 @@ include 'includes/config.inc.php';
     <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="assets/css/style1.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="assets/css/index.css?v=<?php echo time(); ?>">
-
+    <link rel="icon" type="image/x-icon" href="logo.png">
+    
     <!-- font awesome icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
